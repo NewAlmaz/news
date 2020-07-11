4213b12ff269c80342037c2fb48683c8d2bc7790
NewAlmaz <55747879+NewAlmaz@users.noreply.github.com>
2020-07-11 20:06:54 +0300

    Updates

---
+++ precache-manifest.36a17f547f4999063521f713a7115ee3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef5a1a816c6ad872012e10eec6fee72",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "8d034b1dbcf15334dfa3",
+    "url": "/news/static/css/main.59e15f07.chunk.css"
+  },
+  {
+    "revision": "a15525528221592ebdc8",
+    "url": "/news/static/js/2.ce84230f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news/static/js/2.ce84230f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d034b1dbcf15334dfa3",
+    "url": "/news/static/js/main.be2dcf29.chunk.js"
+  },
+  {
+    "revision": "7576b4ad0cd43891079c",
+    "url": "/news/static/js/runtime-main.7ab2d7f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/news/static/media/logo.5d5d9eef.svg"
+  }
+]);
