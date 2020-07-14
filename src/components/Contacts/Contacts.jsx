c4f57db27f9271f9056b19f02d59e237cb25028c
NewAlmaz <55747879+NewAlmaz@users.noreply.github.com>
2020-07-14 14:19:26 +0300

    Страница Контакты готова.

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import "./Contacts.scss"
 
 const Contacts = () => {
     return (
-        <div>Контакты страница</div>
+        <div className="info">
+            <ul className="infoList">
+                <li className="phone">+375 (29) 123 45 76</li>
+                <li className="fio">Пчёлко Алексей</li>
+                <li className="email">PsyDemon@yandex.by</li>
+                <li className="description">JavaScript разработчик</li>
+                <li className="skill">ES5, ES6, React</li>
+            </ul>
+            <img className="photo" src="https://decider.com/wp-content/uploads/2017/06/gilfoyle.jpg?quality=90&strip=all&w=1156&h=771&crop=1"/>
+        </div>
     )
 }
 
