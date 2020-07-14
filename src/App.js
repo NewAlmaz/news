1c02ef1e5caf14aa416b933a663cb7019e20e121
NewAlmaz <55747879+NewAlmaz@users.noreply.github.com>
2020-07-14 15:34:53 +0300

    Страница Контакты готова.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Contacts from "./components/Contacts/Contacts";
 import OneNews from "./components/OneNews/OneNews";
 import AllNews from "./components/AllNews/AllNews";
 import Main from "./components/Main/Main";
-import {BrowserRouter, HashRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
-const App = (props) => {
+const App = () => {
     return (
         <BrowserRouter basename="/news123">
             <div className="App">
@@ -22,7 +22,7 @@ const App = (props) => {
                 </div>
             </div>
         </BrowserRouter>
-    );
-}
+    )
+};
 
 export default App;
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -4,16 +4,22 @@ import "./Contacts.scss"
 const Contacts = () => {
     return (
         <div className="info">
+
             <ul className="infoList">
-                <li className="phone">+375 (29) 123 45 76</li>
+                <li className="phone">+375 (29) 123 45 67</li>
                 <li className="fio">Пчёлко Алексей</li>
                 <li className="email">PsyDemon@yandex.by</li>
                 <li className="description">JavaScript разработчик</li>
                 <li className="skill">ES5, ES6, React</li>
             </ul>
-            <img className="photo" src="https://decider.com/wp-content/uploads/2017/06/gilfoyle.jpg?quality=90&strip=all&w=1156&h=771&crop=1"/>
+
+            <img className="photo"
+                 src="https://decider.com/wp-content/uploads/2017/06/gilfoyle.jpg?quality=90&strip=all&w=1156&h=771&crop=1"
+                 alt="Моя фото"
+            />
+
         </div>
     )
-}
+};
 
 export default Contacts;
